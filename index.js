3c145168c44e05e4a5cd7eae529c0d90c8f56c25
Snef <38905368+Snefff@users.noreply.github.com>
2018-08-24 17:16:42 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -277,15 +277,14 @@ server.post('/addEntityMovie', function (request, response) {
             }
             var url2 = "https://www.gogowego.com/api/v1/Entities";
             var resp = unirest("POST",url2);
-            resp.header('authorization','b897fe23-c8f9-4d98-ba28-fef92e99b96c');
             resp.headers({
-                'authorization': 'b897fe23-c8f9-4d98-ba28-fef92e99b96c',
+                'Authorization': 'Bearer b897fe23-c8f9-4d98-ba28-fef92e99b96c',
                 'Content-type': 'application/json'
               })
             resp.send(JSON.stringify([{
                 "name" : "testGenre",
-                "automaticallyExtensible": false,
-                "useSynonyms" : false,
+                "automatically_extensible": false,
+                "use_synonyms" : false,
                 "data" : output
             }]));
             console.log(resp);
@@ -298,6 +297,75 @@ server.post('/addEntityMovie', function (request, response) {
     })
 })
 
+server.post('/getNews', function (request, response) {
+    console.log(request.body);
+    console.log(request.body.intent.inputs);
+    var url = "https://newsapi.org/v2/"
+        + (request.body.intent.inputs['top-headline'] != ""
+            || request.body.intent.inputs['source'] == "" ? "top-headlines" : "everything")
+        + "?apiKey=" + apiKey;
+    var req = unirest("GET", url);
+    req.query({
+        "pageSize": "4",
+        "page": request.body.intent.inputs['page'] || "1",
+    });
+    (request.body.intent.inputs['top-headline'] != "" || request.body.intent.inputs['source'] == "" ?
+        request.body.intent.inputs['category'] ?
+            req.query({
+                "category": request.body.intent.inputs['category'],
+                "country": request.body.intent.inputs['language'] || "fr"
+            })
+            :
+            req.query({
+                "country": request.body.intent.inputs['language'] || "fr"
+            })
+        : 
+            req.query({
+            "sources": request.body.intent.inputs['source'],
+            "language": request.body.intent.inputs['language'] || "fr"
+        }));
+    console.log(req);
+    req.send("{}");
+    req.end(function (res) {
+        if (res.error) {
+            console.log(res.error);
+            response.setHeader('Content-Type', 'application/json');
+            response.send(JSON.stringify({
+                "speech": "Error. Can you try it again ? ",
+                "displayText": "Error. Can you try it again ? "
+            }));
+        } else if (res.body.totalResults > 0) {
+            let article = res.body.articles;
+            let text = "Voici les news";
+            if (request.body.intent.inputs['source'] != "" && request.body.intent.inputs['source'] != undefined) {
+                text += " de " + request.body.intent.inputs['source'];
+            } else if (request.body.intent.inputs['category'] != "") {
+                text += " correspondates"
+            }
+            text += " :\n";
+            let output = Array(article.length);
+            for (let i = 0; i < article.length; i++) {
+                output[i] = {
+                    "type": "card",
+                    "title": article[i].title,
+                    "image": article[i].urlToImage,
+                    "subtitle": article[i].description,
+                    "buttons": [{
+                        "type": "link",
+                        "text": "Voir en détail",
+                        "value": article[i].url
+                    }]
+                };
+            }
+
+            response.setHeader('Content-Type', 'application/json');
+            response.send(JSON.stringify({
+                "speech": text,
+                "posts": output
+            }));
+        }
+    });
+});
 
 
 server.listen(port, function () {
